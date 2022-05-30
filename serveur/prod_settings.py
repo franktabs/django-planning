40d06dcc29f4863d891902be38403f63b6fe27ce
Junior <tabuguiafrank@gmail.com>
2022-05-30 02:56:27 +0100

    ajout prod_settings

---
+++ serveur/prod_settings.py
@@ -0,0 +1,11 @@
+from re import TEMPLATE
+from .settings import *
+
+DEBUG = False
+
+TEMPLATE_DEBUG = False
+
+SECRET_KEY = "d!27&+0*fw30)6=0w6x)5(6(a0y_oah+47kf-d6gjr82e^0jm("
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
+
