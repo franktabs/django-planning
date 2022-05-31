dbdfa16220fed8e26137c66151b304aa47c97a03
Junior <tabuguiafrank@gmail.com>
2022-05-31 13:16:20 +0100

    ajout host planning-ap

--- serveur/prod_settings.py
+++ serveur/prod_settings.py
@@ -8,7 +8,7 @@ TEMPLATE_DEBUG = True
 
 SECRET_KEY = "d!27&+0*fw30)6=0w6x)5(6(a0y_oah+47kf-d6gjr82e^0jm("
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://planning-ap.herokuapp.com']
 
 DATABASES['default']= dj_database_url.config()
 
