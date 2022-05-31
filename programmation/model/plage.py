d78e9bca12e0ba0942c2f5353b3a2ad5047722e1
Junior <tabuguiafrank@gmail.com>
2022-05-31 03:24:46 +0100

    refactor(model): redéfinition de la clé étrangère vers plage

--- programmation/model/plage.py
+++ programmation/model/plage.py
@@ -1,9 +1,19 @@
 from django.db import models
 from rest_framework import serializers
+from firstApi.model.salle import Salle, SalleSerializer
+from firstApi.model.utilisateur import Enseignant, EnseignantSerializer
+from programmation.model.classes_groupe import Classe, ClasseSerializer
+from programmation.model.ue import Ue, UeSerializer
 
 class Plage(models.Model):
     jour = models.CharField(max_length=45, null=False)
     periode = models.CharField(max_length=45, null=False)
+    
+    
+    enseignants= models.ManyToManyField(Enseignant, through='CoursProgramme', related_name='enseignants')
+    ues = models.ManyToManyField(Ue, through='CoursProgramme', related_name='plages')
+    salles= models.ManyToManyField(Salle, through='CoursProgramme', related_name='plages')
+    classes = models.ManyToManyField(Classe, through='CoursProgramme', related_name='plages')
     
     class Meta:
         db_table = 'plages'
@@ -11,6 +21,10 @@ class Plage(models.Model):
         
 
 class PlageSerializer(serializers.ModelSerializer):
+    enseignants= EnseignantSerializer(read_only=True)
+    ues = UeSerializer(read_only=True)
+    salles= SalleSerializer(read_only=True)
+    classes = ClasseSerializer(read_only=True)
     
     class Meta:
         model = Plage
--- programmation/model/ue.py
+++ programmation/model/ue.py
@@ -1,10 +1,5 @@
 from django.db import models
 from rest_framework import serializers
-from firstApi.model.salle import Salle, SalleSerializer
-from firstApi.model.utilisateur import Enseignant, EnseignantSerializer
-
-from programmation.model.classes_groupe import Classe, ClasseSerializer
-from programmation.model.plage import Plage, PlageSerializer
 
 class Ue(models.Model):
     code = models.CharField(max_length=45, null=False, unique=True)
@@ -13,19 +8,12 @@ class Ue(models.Model):
     # classes = models.ManyToManyField(Classe, through='Enseigne', related_name='ues')
     # enseignants = models.ManyToManyField(Enseignant, through='Enseigne', related_name='ues')
     
-    enseignants= models.ManyToManyField(Enseignant, through='CoursProgramme', related_name='enseignants')
-    plages = models.ManyToManyField(Plage, through='CoursProgramme', related_name='enseignants')
-    salles= models.ManyToManyField(Salle, through='CoursProgramme', related_name='enseignants')
-    classes = models.ManyToManyField(Classe, through='CoursProgramme', related_name='enseignants')
+    
     class Meta:
         db_table = 'ues'
         
 
 class UeSerializer(serializers.ModelSerializer):
-    enseignants= EnseignantSerializer(read_only=True)
-    plages = PlageSerializer(read_only=True)
-    salles= SalleSerializer(read_only=True)
-    classes = ClasseSerializer(read_only=True)
     
     class Meta:
         model = Ue
