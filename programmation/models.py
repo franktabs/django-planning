254d205782e5aa3f2b589e1e81a1bc92fcd1c33b
Junior <tabuguiafrank@gmail.com>
2022-06-01 06:51:08 +0100

    refactor(classe): affichage information par classe

--- programmation/models.py
+++ programmation/models.py
@@ -48,13 +48,49 @@ class GroupeSerializer(serializers.ModelSerializer):
     
 # _____________________________________________________________________________________________________
 
+class Plage(models.Model):
+    jours = (('LUNDI', 'LUNDI'), ('MARDI', 'MARDI'), ('MERCREDI', 'MERCREDI'), ('JEUDI', 'JEUDI'), ('VENDREDI', 'VENDREDI'), ('SAMEDI', 'SAMEDI'), ('DIMANCHE', 'DIMANCHE'))
+    heures = (('7h00 - 9h55', '7h00 - 9h55'), ('10h05 - 12h55', '10h05 - 12h55' ), ('13h05 - 15h55', '13h05 - 15h55'), ('16h05 - 18h55', '16h05 - 18h55'), ('19h05 - 21h55', '19h05 - 21h55'))
+    # code = models.PositiveIntegerField(null=True, blank=True)
+    jour = models.CharField(max_length=45, null=False, choices=jours)
+    periode = models.CharField(max_length=45, null=False, choices=heures)
+    # enseignants= models.ManyToManyField(Enseignant, through='CoursProgramme', related_name='plages')
+    # ues = models.ManyToManyField(Ue, through='CoursProgramme', related_name='plages')
+    # salles= models.ManyToManyField(Salle, through='CoursProgramme', related_name='plages')
+    # classes = models.ManyToManyField(Classe, through='CoursProgramme', related_name='plages')
+    
+    
+    def __str__(self):
+        return  '{} {}'.format(self.jour, self.periode) 
+    
+    class Meta:
+        db_table = 'plages'
+        unique_together = (('jour', 'periode'))
+        
+
+class PlageSerializer(serializers.ModelSerializer):
+    # enseignants= EnseignantSerializer(many=True)
+    # ues = UeSerializer(many=True)
+    # salles= SalleSerializer(many=True)
+    # classes = ClasseSerializer(many=True)
+    
+    class Meta:
+        model = Plage
+        fields = "__all__"
 
 # _______________________________________________________________________________________________________
+
 class Classe(models.Model):
     code = models.CharField(unique=True, max_length=45, null=False)
     effectif = models.IntegerField()
     departements = models.ForeignKey(Departement, on_delete=models.PROTECT)
     groupes = models.ManyToManyField(Groupe, through="ClassesGroupe", related_name='classes')
+    
+    enseignants= models.ManyToManyField(Enseignant, through='CoursProgramme', related_name='classes')
+    ues = models.ManyToManyField(Ue, through='CoursProgramme', related_name='classes')
+    salles= models.ManyToManyField(Salle, through='CoursProgramme', related_name='classes')
+    plages = models.ManyToManyField('Plage', through='CoursProgramme', related_name='classes')
+    
     
     class Meta:
         db_table = 'classes'
@@ -66,6 +102,10 @@ class Classe(models.Model):
 class ClasseSerializer(serializers.ModelSerializer):
     departements = DepartementSerializer(read_only=True)
 
+    enseignants= EnseignantSerializer(many=True)
+    ues = UeSerializer(many=True)
+    salles= SalleSerializer(many=True)
+    plages = PlageSerializer(many=True)
     class Meta:
         model = Classe
         fields = "__all__"
@@ -109,36 +149,6 @@ class SpecialiteSerializer(serializers.ModelSerializer):
         model = Specialite
         fields = '__all__'
 # _______________________________________________________________________
-
-class Plage(models.Model):
-    jours = (('LUNDI', 'LUNDI'), ('MARDI', 'MARDI'), ('MERCREDI', 'MERCREDI'), ('JEUDI', 'JEUDI'), ('VENDREDI', 'VENDREDI'), ('SAMEDI', 'SAMEDI'), ('DIMANCHE', 'DIMANCHE'))
-    heures = (('7h00 - 9h55', '7h00 - 9h55'), ('10h05 - 12h55', '10h05 - 12h55' ), ('13h05 - 15h55', '13h05 - 15h55'), ('16h05 - 18h55', '16h05 - 18h55'), ('19h05 - 21h55', '19h05 - 21h55'))
-    # code = models.PositiveIntegerField(null=True, blank=True)
-    jour = models.CharField(max_length=45, null=False, choices=jours)
-    periode = models.CharField(max_length=45, null=False, choices=heures)
-    enseignants= models.ManyToManyField(Enseignant, through='CoursProgramme', related_name='plages')
-    ues = models.ManyToManyField(Ue, through='CoursProgramme', related_name='plages')
-    salles= models.ManyToManyField(Salle, through='CoursProgramme', related_name='plages')
-    classes = models.ManyToManyField(Classe, through='CoursProgramme', related_name='plages')
-    
-    
-    def __str__(self):
-        return  '{} {}'.format(self.jour, self.periode) 
-    
-    class Meta:
-        db_table = 'plages'
-        unique_together = (('jour', 'periode'))
-        
-
-class PlageSerializer(serializers.ModelSerializer):
-    enseignants= EnseignantSerializer(many=True)
-    ues = UeSerializer(many=True)
-    salles= SalleSerializer(many=True)
-    classes = ClasseSerializer(many=True)
-    
-    class Meta:
-        model = Plage
-        fields = "__all__"
 
 # _______________________________________________________________________
 
