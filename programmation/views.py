f9ca258a562ecf68731bf15c68de38858a7d665d
Junior <tabuguiafrank@gmail.com>
2022-06-02 20:12:49 +0100

    opt(viewset): methode get de classe detail

--- programmation/views.py
+++ programmation/views.py
@@ -29,6 +29,46 @@ class CoursProgrammeViewSet(ModelViewSet):
     serializer_class = CoursProgrammeSerializer
     queryset = CoursProgramme.objects.all()
 
+    def retrieve(self, request, *args, **kwargs):
+        cours_programme = CoursProgramme.objects.filter(classes_id=kwargs['pk'])
+        serializer = self.serializer_class(cours_programme, many=True)
+        datas = serializer.data
+        tab = []
+        classes = []
+        for i in datas:
+
+            if i['classes']:
+                k = -1
+                existes = False
+                for j in classes:
+                    k += 1
+                    if i['classes']['code'] == j:
+                        existes = True
+                        tab[k]['cours'].append({
+                            'plage': i['plages']['id'] - 2,
+                            'ue': i['ues']['code'],
+                            'enseignant': i['enseignants']['noms'],
+                            'salle': i['salles']['code'],
+                        })
+                        break
+                    
+                if not existes :
+                    
+                    myjson = {}
+                    myjson['id'] = i['id']
+                    myjson['codeClasse'] = i['classes']['code']
+                    classes.append(i['classes']['code'])
+                    myjson['cours'] = [
+                        {
+                            'plage': i['plages']['id'] - 2,
+                            'ue': i['ues']['code'],
+                            'enseignant': i['enseignants']['noms'],
+                            'salle': i['salles']['code'],
+                        }
+                    ]
+                    tab.append(myjson)
+        return Response(['Executer'])
+
     def list(self, request, *args, **kwargs):
         serializer = self.serializer_class(self.queryset, many=True)
         datas = serializer.data
