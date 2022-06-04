fbc5c38144c80fa97cbda4e6b37af0c1f924149a
Junior <tabuguiafrank@gmail.com>
2022-06-04 08:40:04 +0100

    fix(url): fixation sur l'erreur cours programme

--- programmation/views.py
+++ programmation/views.py
@@ -34,90 +34,90 @@ class CoursProgrammeViewSet(ModelViewSet):
     serializer_class = CoursProgrammeSerializer
     queryset = CoursProgramme.objects.all()
 
-    def retrieve(self, request, *args, **kwargs):
+    # def retrieve(self, request, *args, **kwargs):
         
-        cours_programme = CoursProgramme.objects.filter(
-            classes_id=kwargs['pk'])
-        serializer = self.serializer_class(cours_programme, many=True)
+    #     cours_programme = CoursProgramme.objects.filter(
+    #         classes_id=kwargs['pk'])
+    #     serializer = self.serializer_class(cours_programme, many=True)
 
-        datas = serializer.data
-        tab = []
-        classes = []
-        for i in datas:
+    #     datas = serializer.data
+    #     tab = []
+    #     classes = []
+    #     for i in datas:
 
-            if i['classes']:
-                k = -1
-                existes = False
-                for j in classes:
-                    k += 1
-                    if i['classes']['code'] == j:
-                        existes = True
-                        tab[k]['cours'].append({
-                            'plage': i['plages']['id'] - 2,
-                            'ue': i['ues']['code'],
-                            'enseignant': i['enseignants']['noms'],
-                            'salle': i['salles']['code'],
-                        })
-                        break
+    #         if i['classes']:
+    #             k = -1
+    #             existes = False
+    #             for j in classes:
+    #                 k += 1
+    #                 if i['classes']['code'] == j:
+    #                     existes = True
+    #                     tab[k]['cours'].append({
+    #                         'plage': i['plages']['id'] - 2,
+    #                         'ue': i['ues']['code'],
+    #                         'enseignant': i['enseignants']['noms'],
+    #                         'salle': i['salles']['code'],
+    #                     })
+    #                     break
 
-                if not existes:
+    #             if not existes:
 
-                    myjson = {}
-                    myjson['id'] = i['id']
-                    myjson['classes'] = i['classes']['id']
-                    myjson['codeClasse'] = i['classes']['code']
-                    classes.append(i['classes']['code'])
-                    myjson['cours'] = [
-                        {
-                            'plage': i['plages']['id'] - 2,
-                            'ue': i['ues']['code'],
-                            'enseignant': i['enseignants']['noms'],
-                            'salle': i['salles']['code'],
-                        }
-                    ]
-                    tab.append(myjson)
-        return Response(tab, 200)
+    #                 myjson = {}
+    #                 myjson['id'] = i['id']
+    #                 myjson['classes'] = i['classes']['id']
+    #                 myjson['codeClasse'] = i['classes']['code']
+    #                 classes.append(i['classes']['code'])
+    #                 myjson['cours'] = [
+    #                     {
+    #                         'plage': i['plages']['id'] - 2,
+    #                         'ue': i['ues']['code'],
+    #                         'enseignant': i['enseignants']['noms'],
+    #                         'salle': i['salles']['code'],
+    #                     }
+    #                 ]
+    #                 tab.append(myjson)
+    #     return Response(tab, 200)
 
-    def list(self, request, *args, **kwargs):
-        serializer = self.serializer_class(self.queryset, many=True)
-        datas = serializer.data
-        tab = []
-        classes = []
-        for i in datas:
+    # def list(self, request, *args, **kwargs):
+    #     serializer = self.serializer_class(self.queryset, many=True)
+    #     datas = serializer.data
+    #     tab = []
+    #     classes = []
+    #     for i in datas:
 
-            if i['classes']:
-                k = -1
-                existes = False
-                for j in classes:
-                    k += 1
-                    if i['classes']['code'] == j:
-                        existes = True
-                        tab[k]['cours'].append({
-                            'plage': i['plages']['id'] - 2,
-                            'ue': i['ues']['code'],
-                            'enseignant': i['enseignants']['noms'],
-                            'salle': i['salles']['code'],
-                        })
-                        break
+    #         if i['classes']:
+    #             k = -1
+    #             existes = False
+    #             for j in classes:
+    #                 k += 1
+    #                 if i['classes']['code'] == j:
+    #                     existes = True
+    #                     tab[k]['cours'].append({
+    #                         'plage': i['plages']['id'] - 2,
+    #                         'ue': i['ues']['code'],
+    #                         'enseignant': i['enseignants']['noms'],
+    #                         'salle': i['salles']['code'],
+    #                     })
+    #                     break
 
-                if not existes:
+    #             if not existes:
 
-                    myjson = {}
-                    myjson['id'] = i['id']
-                    myjson['classes'] = i['classes']['id']
-                    myjson['codeClasse'] = i['classes']['code']
-                    classes.append(i['classes']['code'])
-                    myjson['cours'] = [
-                        {
-                            'plage': i['plages']['id'] - 2,
-                            'ue': i['ues']['code'],
-                            'enseignant': i['enseignants']['noms'],
-                            'salle': i['salles']['code'],
-                        }
-                    ]
-                    tab.append(myjson)
+    #                 myjson = {}
+    #                 myjson['id'] = i['id']
+    #                 myjson['classes'] = i['classes']['id']
+    #                 myjson['codeClasse'] = i['classes']['code']
+    #                 classes.append(i['classes']['code'])
+    #                 myjson['cours'] = [
+    #                     {
+    #                         'plage': i['plages']['id'] - 2,
+    #                         'ue': i['ues']['code'],
+    #                         'enseignant': i['enseignants']['noms'],
+    #                         'salle': i['salles']['code'],
+    #                     }
+    #                 ]
+    #                 tab.append(myjson)
 
-        return Response(tab, 200)
+    #     return Response(tab, 200)
 
 
 class ClasseViewSet(ModelViewSet):
